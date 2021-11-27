74b57efce4c0c243abddcaa888b3c6645f46394f
programmer-offbeat <87474641+programmer-offbeat@users.noreply.github.com>
2021-11-27 19:30:55 +0530

    Update about.js

--- src/commands/Information/about.js
+++ src/commands/Information/about.js
@@ -18,24 +18,24 @@ module.exports = {
     .setStyle("LINK")
     .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot`),
 			new MessageButton()
-    .setLabel("GitHub")
-    .setStyle("LINK")
-    .setURL("https://github.com/brblacky/lavamusic"),
+    .setLabel("i suck")
+    //.setStyle("LINK")
+    //.setURL("https://github.com/brblacky/lavamusic"),
     new MessageButton()
     .setLabel("Support")
     .setStyle("LINK")
-    .setURL("https://discord.gg/gfcv94hDhv")
+    .setURL("https://discord.gg/trades")
 			);
 
       const mainPage = new MessageEmbed()
             .setAuthor('LavaMusic', 'https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png')
             .setThumbnail('https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png')
             .setColor('#303236')
-            .addField('Creator', '[Blacky#6618](https://github.com/brblacky) And [Venom#9718](https://github.com/Venom9718/)', true)
-            .addField('Organization', '[Blacky](https://github.com/brblacky)', true)
-            .addField('Repository', '[Here](https://github.com/brblacky/lavamusic)', true)
+            .addField('Creator', '[offbeat](https://offbeat-s.carrd.co)', true)
+            //.addField('Organization', '[Blacky](https://github.com/brblacky)', true)
+            .addField('fuck you', 'ah shit', true)
             .addField('\u200b',
-                `[LavaMusic](https://github.com/brblacky/lavamusic/) is [Blacky](https://github.com/brblacky) and [Venom](https://github.com/Venom9718)'s Was created by blacky and Venom. He really wants to make his first open source project ever. Because he wants more for coding experience. In this project, he was challenged to make project with less bugs. Hope you enjoy using LavaMusic!`
+                `to make aesthetic`
             )
         return message.reply({embeds: [mainPage], components: [row]});
     }
