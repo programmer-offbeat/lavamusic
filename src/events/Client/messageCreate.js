23b99d8bc584d3b3797b0e0987e2cebf9e7b4f64
programmer-offbeat <87474641+programmer-offbeat@users.noreply.github.com>
2021-12-22 14:43:46 +0530

    Update messageCreate.js

--- src/events/Client/messageCreate.js
+++ src/events/Client/messageCreate.js
@@ -14,7 +14,7 @@ module.exports = async (client, message) => {
     if (message.content.match(mention)) {
       const embed = new MessageEmbed()
         .setColor('#7ba3cf')
-        .setDescription(`<:GH_aPinkArrow:914394929449545759> My prefix in this server is \`${prefix}\`\n <:GH_aPinkArrow:914394929449545759> To see all my commands,use \`${prefix}\`help`)
+        .setDescription(`<a:dp_arroq:923139463226490910> My prefix in this guild is \`${prefix}\`\n <a:dp_arroq:923139463226490910> Use \`${prefix}\`help` to get all commands !`)
         .setFooter(`SyncO Tunes`);
       message.channel.send({embeds: [embed]})
     };
